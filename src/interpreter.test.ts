23dbccf23c0041ee4bf4d81207f19dc85379bf12
Dan Vanderkam <danvdk@gmail.com>
2024-01-12 19:42:42 -0500

    fill out tests

--- src/interpreter.test.ts
+++ src/interpreter.test.ts
@@ -14,4 +14,28 @@ describe("interpreter", () => {
 		expect(evaluate("1 + 2 * 3")).toEqual(7);
 		expect(evaluate("3 - 1 / 2")).toEqual(2.5);
 	});
+
+	it("should evaluate comparison operators", () => {
+		expect(evaluate("12 > 6")).toBe(true);
+		expect(evaluate("12 > 12")).toBe(false);
+		expect(evaluate("12 == 12")).toBe(true);
+		expect(evaluate("12 != 12")).toBe(false);
+		expect(evaluate("0 == nil")).toBe(false);
+		expect(evaluate("nil == nil")).toBe(true);
+		expect(evaluate("2 >= 2")).toBe(true);
+		expect(evaluate("2 >= 3")).toBe(false);
+		expect(evaluate("2 <= 3")).toBe(true);
+		expect(evaluate("2 < 3")).toBe(true);
+	});
+
+	it("should evaluate unary operators", () => {
+		expect(evaluate("-12")).toBe(-12);
+		expect(evaluate("-(1 + 2)")).toBe(-3);
+		expect(evaluate("!nil")).toBe(true);
+		expect(evaluate("!!nil")).toBe(false);
+	});
+
+	it("should concatenate strings", () => {
+		expect(evaluate(`"hello" + " " + "world"`)).toEqual("hello world");
+	});
 });
