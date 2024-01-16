d10fb6a76735bd657a011b8503a95fd582564591
Dan Vanderkam <danvdk@gmail.com>
2024-01-16 12:32:14 -0500

    really no dts

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	bundle: false,
 	clean: true,
-	dts: true,
+	dts: false,
 	entry: ["src/**/*.ts", "!src/**/*.test.*"],
 	format: "esm",
 	outDir: "lib",
