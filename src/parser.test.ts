a350a800d5bc447891ff0d1ed8846821d166ffb1
Dan Vanderkam <danvdk@gmail.com>
2024-01-20 10:49:54 -0500

    unused import

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it } from "vitest";
 
 import { visitStmt } from "./ast.js";
 import { astPrinter } from "./ast-printer.js";
