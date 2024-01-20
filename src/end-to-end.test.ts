e23ac91693ff593d823925421421469429632b39
Dan Vanderkam <danvdk@gmail.com>
2024-01-20 10:49:19 -0500

    test errors in end-to-end

--- src/end-to-end.test.ts
+++ src/end-to-end.test.ts
@@ -3,7 +3,7 @@ import * as fs from "node:fs/promises";
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 
 import { main, resetErrors } from "./main.js";
-import { mockError, mockExit } from "./test-utils.js";
+import { mockError, mockExit, mockLog } from "./test-utils.js";
 
 const testFiles = [
 	"end-of-chapter8.lox",
@@ -21,22 +21,39 @@ const testFiles = [
 	"chapter10-multi-currency.lox",
 	"chapter10-return-in-loop.lox",
 	"chapter10-return-nothing.lox",
+	"chapter10-too-many-params.lox",
 ];
+
+async function maybeReadFile(path: string): Promise<null | string> {
+	try {
+		return await fs.readFile(path, "utf-8");
+	} catch (e) {
+		if (e instanceof Error && "errno" in e && e.errno === -2) {
+			return null;
+		}
+		throw e;
+	}
+}
 
 // Similar to tests in main.test.ts, except that fs isn't mocked.
 describe("end-to-end tests", () => {
 	let stashedArgv = process.argv;
 	let exit: ReturnType<typeof mockExit>;
+	let log: ReturnType<typeof mockLog>;
 	let error: ReturnType<typeof mockError>;
 	let logLines: string[] = [];
+	let errorLines: string[] = [];
 	beforeEach(() => {
 		stashedArgv = process.argv;
 		exit = mockExit();
-		error = mockError();
-		vi.spyOn(console, "log").mockImplementation((line: string) => {
+		log = vi.spyOn(console, "log").mockImplementation((line: string) => {
 			logLines.push(line);
 		});
+		error = vi.spyOn(console, "error").mockImplementation((line: string) => {
+			errorLines.push(line);
+		});
 		logLines = [];
+		errorLines = [];
 		resetErrors();
 	});
 	afterEach(() => {
@@ -45,12 +62,24 @@ describe("end-to-end tests", () => {
 	});
 
 	it.each(testFiles.map((file) => [file]))("baseline: %s", async (filename) => {
-		const baseline = filename.replace(".lox", ".txt");
-		const expected = await fs.readFile(`baselines/${baseline}`, "utf8");
+		const baseline = filename.replace(".lox", "");
+		const expected = await fs.readFile(`baselines/${baseline}.txt`, "utf8");
+		const errors = await maybeReadFile(`baselines/${baseline}.errors.txt`);
 		process.argv = ["node", "gravlax.ts", `examples/${filename}`];
 		await main();
-		expect(exit).not.toHaveBeenCalled();
-		expect(error).not.toHaveBeenCalled();
-		expect(logLines).toEqual(expected.trimEnd().split("\n"));
+
+		if (expected !== "") {
+			expect(logLines).toEqual(expected.trimEnd().split("\n"));
+		} else {
+			expect(log).not.toHaveBeenCalled();
+		}
+
+		if (errors && errors.length > 0) {
+			expect(errorLines).toEqual(errors.trimEnd().split("\n"));
+			expect(exit).toHaveBeenCalledOnce(); // TODO: check exit code
+		} else {
+			expect(error).not.toHaveBeenCalled();
+			expect(exit).not.toHaveBeenCalled();
+		}
 	});
 });
