ec6b738c1dcff7a95c87a1df76029dbdd1de739f
Dan Vanderkam <danvdk@gmail.com>
2024-03-16 20:17:42 -0700

    break up Value

--- src/bytecode/value.ts
+++ src/bytecode/value.ts
@@ -1,7 +1,31 @@
-export type Value = number & { __brand: "value" };
+export enum ValueType {
+	Bool,
+	Nil,
+	Number,
+}
+
+interface ValueBase {
+	type: ValueType;
+}
+export interface BoolValue extends ValueBase {
+	type: ValueType.Bool;
+	as: boolean;
+}
+export interface NumberValue extends ValueBase {
+	type: ValueType.Number;
+	as: number;
+}
+export interface NilValue extends ValueBase {
+	type: ValueType.Nil;
+}
+export type Value = BoolValue | NilValue | NumberValue;
+
+export function boolValue(value: boolean): BoolValue {
+	return { as: value, type: ValueType.Bool };
+}
+export const nilValue: NilValue = { type: ValueType.Nil };
+export function numberValue(value: number): NumberValue {
+	return { as: value, type: ValueType.Number };
+}
 
 // No need to implement ValueArray; it's just Value[].
-
-export function Value(n: number): Value {
-	return n as Value;
-}
